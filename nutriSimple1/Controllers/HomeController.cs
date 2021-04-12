bbdef1132c2de3dc562fb4265f5f59678c48ccd3
Zack <saidgwx@gmail.com>
2021-04-12 13:05:34 -0400

    Ajoutez des fichiers projet.

---
+++ nutriSimple1/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace nutriSimple1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Rendezvous()
+        {
+            ViewBag.Message = "Gestions des RENDEZ-VOUS.";
+
+            return View();
+        }
+        public ActionResult DeplacerRDV()
+        {
+            ViewBag.Message = "PLANIFIER/DÉPLACER VOS RENDEZ-VOUS.";
+
+            return View();
+        }
+        public ActionResult AutresRDV()
+        {
+            ViewBag.Message = "Autres RENDEZ-VOUS.";
+
+            return View();
+        }
+    }
+}
---
+++ nutriSimple1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="nutriSimple1.MvcApplication" Language="C#" %>
